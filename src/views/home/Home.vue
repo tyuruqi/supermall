db25ff127bb3f5e450920947527b3b203a6a9bf4
qaz <tyuruqi@hotmail.com>
2022-06-30 14:47:14 +0800

    首页轮播图，首页推荐栏

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends"/>
+  </div>
 </template>
 
 <script>
+
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+
+import {getHomeMultidata} from "network/home";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data(){
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  //首页组件创建完成后发送网络请求获取首页数据
+  created() {
+    //1、请求多个数据  异步操作
+    getHomeMultidata().then(res=> {
+      //当前this只想当前对象，箭头函数的this是找上一层的this，组件内created的this就是当前组件
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: var(--color-background);
+}
 </style>
